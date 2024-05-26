290fa14f0e0a21780388b0a1176b1e8a0d64027c
marizal-akib <marizalakib@gmail.com>
2024-05-26 22:28:28 +0100

    deploy again on vercel aj brainserver

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express = require('express');
-var jwt = require('jsonwebtoken');
 const app = express()
 const cors = require('cors')
+const { ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
-app.use(cors());
 
-middleware
+
+// middleware
+// app.use(cors());
 app.use(cors({
     origin: [
-        "http://localhost:5173" ,
-      
+        "http://localhost:5173",
+        "https://neuropath-d1a3c.web.app"
     ],
     credentials: true,
     optionSuccessStatus: 200,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
 }));
 app.use(express.json());
-
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.pg0uvv0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -41,12 +41,32 @@ async function run() {
         const patientCollection = client.db('neuronDB').collection('patient')
 
 
+
+
         // patient api
         app.post('/newPatient', async (req, res) => {
             const newPatient = req.body
             const result = await patientCollection.insertOne(newPatient);
             res.send(result);
         })
+
+        app.get('/patient', async (req, res) => {
+            const result = await patientCollection.find().toArray();
+            res.send(result);
+        })
+        app.get('/patients', async (req, res) => {
+            const id = req.query?.id
+            let query = {}
+            if (req.query?.id) {
+                query = { _id: new ObjectId(id) }
+            }
+            console.log(query);
+            const cursor = patientCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
